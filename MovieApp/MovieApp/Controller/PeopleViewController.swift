54fd3b8dd92c26da39435112aa249bdf379ca74d
kirilovep <kirilovep@gmail.com>
2020-10-05 14:55:06 +0300

    add people model and outlets

--- MovieApp/MovieApp/Controller/PeopleViewController.swift
+++ MovieApp/MovieApp/Controller/PeopleViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class PeopleViewController: UIViewController {
 
+    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var placeOfBirthdayLabel: UILabel!
+    @IBOutlet weak var imagesCollectionView: UICollectionView!
+    @IBOutlet weak var knowsForLabel: UILabel!
+    @IBOutlet weak var biographyLabel: UILabel!
+    @IBOutlet weak var moviesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
